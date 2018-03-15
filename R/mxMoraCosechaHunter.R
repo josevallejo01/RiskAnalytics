289164a3df20a86924254d0531dfa4315deea3bc
José Miguel Vallejo Alarcón <mvallejo@cajasipan.com.pe>
2018-03-15 10:35:18 -0500

    Se incluye variable nNumPerTol que fue omitida inicialmente.

--- R/mxMoraCosechaHunter.R
+++ R/mxMoraCosechaHunter.R
@@ -1,17 +1,17 @@
 #' Buscador de segmentos que presentan degradación continua en el indicador de mora ajustada por cosechas
 #'
-#' Construye un cuadro con los segmentos que han superado un umbral (nTolerancia) de mora 
+#' Construye un cuadro con los segmentos que han superado un umbral (nTolerancia) de mora
 #' de manera continua en un conjunto de meses (nNumPerTol). La búsqueda se hace a lo largo
-#' de una ventana temporal de observación (nOrder). 
+#' de una ventana temporal de observación (nOrder).
 #' La búsqueda sigue un algoritmo combinatorio. Dado un vector de segmentos a analizar (vSegmento)
 #' y un número establecido de segmentos a tomar en cuenta para la formación de cada combinación,
 #' se construye un conjunto de combinaciones de segmentos en los que se
 #' buscará que se cumplan los criterios mencionados anteriormente. De especificarse que las combinaciones
 #' sean acumulativas (bCumDepth), se construirá progresivamente un conjunto de combinaciones que contendrá
 #' inicialmente a cada segmentos hasta contener a las combinaciones entre todos los segmentos.
-#' Además del marco de datos con información de los resultados de la búsqueda, se genera los data.frame 
+#' Además del marco de datos con información de los resultados de la búsqueda, se genera los data.frame
 #' "dfMontoDesemSegmentos", "dfNumCreDesemSegmentos" con el detalle de desembolsos y número de créditos desembolsados
-#' manteniendo la misma estructura. Adicionalmente se genera el data.frame de validación "dfValid" el cual presenta 
+#' manteniendo la misma estructura. Adicionalmente se genera el data.frame de validación "dfValid" el cual presenta
 #' las posiciones en donde se cumplio con los criterios de búsqueda.
 #' @param handle integer: Objeto de clase RODBC que establece la conexión al servidor.
 #' @param dfBase data.frame: Marco de datos conteniendo como mínimo el código de crédito (NUM_CRE) y los segmentos de análisis.
@@ -22,9 +22,10 @@
 #' @param nPeriodo numeric: Número de meses de agrupación para formar grupos temporales (mensuales, bimestrales, etc.). Por defecto es 3 (trimestral).
 #' @param nDepth numeric: Número de segmentos que debe contener cada combinación. Por defecto es 2.
 #' @param nOrder numeric: Tamaño de la ventana de análisis cuyo punto de referencia inicial es el último periodo disponible. Por defecto es 3.
-#' @param bCumDepth boolean: TRUE si la cantidad de segmentos a considerar para la construcción de combinaciones debe ser incremental. 
+#' @param bCumDepth boolean: TRUE si la cantidad de segmentos a considerar para la construcción de combinaciones debe ser incremental.
 #' El límite está establecido por la variable nDepth. Por defecto es FALSE.
 #' @param nTolerancia numeric: Umbral de referencia para la identificación de segmentos de elevado riesgo.
+#' @param nNumPerTol numeric: Número de meses en los que se busca que se supere la tolerancia.
 #' @param batch numeric: Tamaño del lote. Por defecto es 1,500.
 #' @param bExportTab logical: Indica si el marco de datos resultante debe ser exportado en un fichero.
 #' @param cNomTab character: En caso se exporte el marco de datos, nombre del fichero.
@@ -33,11 +34,11 @@
 #' @export mxHunter
 #' @return data.frame
 #' @examples
-#' 
+#'
 #' # Carga la base dentro del entorno R
 #' base <- mxBase()
-#' 
-#' tab <- mxMoraCosechaHunter(handle = dbhandle, dfBase = base, 
+#'
+#' tab <- mxMoraCosechaHunter(handle = dbhandle, dfBase = base,
 #'                            cFecIni =  "2016-01-01", cFecFin = "2018-02-28",
 #'                            vSegmento = c("cod_ofi_inf", "nivel", "tip_cre_des"),
 #'                            nDepth = 2, bCumDepth = TRUE, nTolerancia = 0.018,
@@ -45,13 +46,14 @@
 
 mxMoraCosechaHunter <- function(handle, dfBase, cFecIni, cFecFin,
                                 vSegmento, nMadurez = 6, nPeriodo = 3, nDepth = 2,
-                                nOrder = 3, bCumDepth = FALSE, nTolerancia = 0.18, batch = 1500,
+                                nOrder = 3, bCumDepth = FALSE, nTolerancia = 0.18,
+                                nNumPerTol, batch = 1500,
                                 bExportTab = FALSE, cNomTab){
   library(RODBC)
-  library(dplyr) 
-  
+  library(dplyr)
+
   if(nNumPerTol > nOrder){return("nNumPerTol no puede superar nOrder")}
-  
+
   # Segmentos de análisis
   # matCombn <- list(combn(length(vSegmento), nDepth))
   library(zoo)
@@ -67,7 +69,7 @@ mxMoraCosechaHunter <- function(handle, dfBase, cFecIni, cFecFin,
   dfMontoDesemSegmentos <- data.frame()
   dfNumCreDesemSegmentos <- data.frame()
   dfSegmentos <- data.frame()
-  
+
   for(i in 1:length(matCombn)){
     for(j in 1:ncol(matCombn[[i]])){
       dfSegmentos <- bind_rows(dfSegmentos,
@@ -79,22 +81,22 @@ mxMoraCosechaHunter <- function(handle, dfBase, cFecIni, cFecFin,
                                                             nMadurez = nMadurez,
                                                             nPeriodo = nPeriodo,
                                                             batch = batch))
-      
+
       dfMontoDesemSegmentos <- bind_rows(dfMontoDesemSegmentos,
                                          dfMontoDesemSegmento)
-      
-      
+
+
       dfNumCreDesemSegmentos <- bind_rows(dfNumCreDesemSegmentos,
                                           dfNumCreDesemSegmento)
-      
+
       #Cambia posición de la última columna si se agrega un nuevo elemento
       if(i == 1 && j > 1){
-        dfSegmentos <- dfSegmentos %>% 
+        dfSegmentos <- dfSegmentos %>%
           select(vSegmento[matCombn[[i]][j]], everything())
-        
+
         dfMontoDesemSegmentos <- dfMontoDesemSegmentos %>%
           select(vSegmento[matCombn[[i]][j]], everything())
-        
+
         dfNumCreDesemSegmentos <- dfNumCreDesemSegmentos %>%
           select(vSegmento[matCombn[[i]][j]], everything())
       }
@@ -105,7 +107,7 @@ mxMoraCosechaHunter <- function(handle, dfBase, cFecIni, cFecFin,
   l<- apply(dfSegmentos,2,function(x)any(!is.na(x)))
   l <- l[l]
   l <- names(l[length(l)])
-  
+
   dfSegmentos <- dfSegmentos[,1:match(l,names(dfSegmentos))]
 
   # Cancela si el número de columnas es menor al  periodos de análisis
@@ -115,7 +117,7 @@ mxMoraCosechaHunter <- function(handle, dfBase, cFecIni, cFecFin,
 
   # Toma la ventana de análisis nOrder
   k <- dfSegmentos[,(ncol(dfSegmentos)-nOrder+1):ncol(dfSegmentos)]
-  
+
   # Revisa segmentos que sobrepasaron la tolerancia contínuamente por un número de
   # periodos nNumPerTol = 3
   p <- apply(k, 1,
@@ -138,56 +140,56 @@ mxMoraCosechaHunter <- function(handle, dfBase, cFecIni, cFecFin,
                1,
                function(x)all(x == FALSE))
     g[is.na(g)] <- TRUE
-    
+
     dfValid <- dfValid[!g,]
     dfFinal <- dfValid[,1:(length(vSegmento)+1+nOrder)]
     dfValid <- bind_cols(dfValid[,1:(length(vSegmento)+1)],
                          dfValid[,(length(vSegmento)+2+nOrder):ncol(dfValid)])
   }
-  
+
   # Genera tablas finales de desembolsos y números operaciones
   dfMontoDesemSegmentos <- dfMontoDesemSegmentos[as.numeric(row.names(dfValid)),]
   dfMontoDesemSegmentos <- dfMontoDesemSegmentos[,c(1:(length(vSegmento)),
                            (match(l,names(dfMontoDesemSegmentos))-nDepth):
                              ncol(dfMontoDesemSegmentos))]
-  
+
   dfNumCreDesemSegmentos <- dfNumCreDesemSegmentos[as.numeric(row.names(dfValid)),]
   dfNumCreDesemSegmentos <- dfNumCreDesemSegmentos[,c(1:(length(vSegmento)),
                                                     (match(l,names(dfNumCreDesemSegmentos))-nDepth):
                                                       ncol(dfNumCreDesemSegmentos))]
-  
+
   if(bExportTab == TRUE){
     library(xtable)
     table <- xtable(x = dfFinal,
                     digits = 2,
                     latex.environments="center")
-    
+
     # Crea directorio
     mainDir <- "./"
     subDir <- "TABLAS"
-    
+
     ifelse(!dir.exists(file.path(mainDir, subDir)),
            dir.create(file.path(mainDir, subDir)), FALSE)
-    
+
     print(table,
           type = "html",
           include.rownames = FALSE,
           file = paste0("./TABLAS/", cNomTab, ".html"))
   }
-  
-  
+
+
   assign(x = 'dfMontoDesemSegmentos',
          value = dfMontoDesemSegmentos,
          envir=.GlobalEnv)
-  
+
   assign(x = 'dfNumCreDesemSegmentos',
          value = dfNumCreDesemSegmentos,
          envir=.GlobalEnv)
-  
+
   assign(x = 'dfValid',
          value = dfValid,
          envir=.GlobalEnv)
-  
+
   return(dfFinal)
 
 }
