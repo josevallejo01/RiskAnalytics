dcf5164ade40f48cdf35abadea1aa20d3c48db8c
José Miguel Vallejo Alarcón <mvallejo@cajasipan.com.pe>
2018-05-11 16:01:22 -0500

    Se agrega indicador de sobreendeudamiento

--- R/mxBase.R
+++ R/mxBase.R
@@ -10,18 +10,13 @@
 mxBase <- function(handle){
   library(RODBC)
   cQueryBase <- "select distinct
-
   -------------------------------------------INFORMATIVAS----------------------------------------------------
-
   NUM_solicitud=cre.NUM_SOL,        --número de solicitud de crédtio
   cre.NUM_CRE,                      --número de crédito
   COD_cliente=cre.cod_cli,          --código de cliente
-
   -------------------------------------------PARA ANÁLISIS---------------------------------------------------
-
   --NÚM DE IFIS
   num_emp = isnull(tab5.num_emp,0),
-
   --NIVEL DE APROBACIÓN / TAMAÑO DE DESEMBOLSO
   cod_nivel = (CASE
   when (case when cre.COD_MON_CRE='01' then cre.mon_des else cre.mon_des*(cast(tc2.tca_fij as decimal(12,6) ))  end )   <=5000 then '001'
@@ -37,35 +32,27 @@ mxBase <- function(handle){
   when (case when cre.COD_MON_CRE='01' then cre.mon_des else cre.mon_des*(cast(tc2.tca_fij as decimal(12,6) ))  end ) <=50000 then 'Gerente de Negocios'
   when (case when cre.COD_MON_CRE='01' then cre.mon_des else cre.mon_des*(cast(tc2.tca_fij as decimal(12,6) ))  end ) <=100000 then 'Gerente General' else 'Directorio'
   END),
-
   --SECTOR ECONÓMICO
   cre.cod_des,
   Sector_economico = eco.des_act_eco,
-
   --PRODUCTO
   cod_producto = (CASE WHEN his.tip_cre_pr='COMERCIAL' or his.tip_cre_pr='DIARIO' or his.tip_cre_pr='MES' THEN '003'
   WHEN his.tip_cre_pr='CONSUMO' or his.tip_cre_pr='MIVIVIENDA' or his.tip_cre_pr='VIVEMEJOR' THEN '002'
   WHEN his.tip_cre_pr='AGRICOLA' THEN '001'  END),
-
   desc_producto = (CASE WHEN his.tip_cre_pr='COMERCIAL' or his.tip_cre_pr='DIARIO' or his.tip_cre_pr='MES' THEN 'MYPE'
   WHEN his.tip_cre_pr='CONSUMO' or his.tip_cre_pr='MIVIVIENDA' or his.tip_cre_pr='VIVEMEJOR' THEN 'CONSUMO'
   WHEN his.tip_cre_pr='AGRICOLA' THEN 'AGRICOLA' 	 END),
-
   --TIPO DE CRÉDITO
   cre.cre_tip_cre_sbs,
   TIPCRE.tip_cre_des,
-
   --MODALIDAD DE FINANCIAMIENTO
   cre.cre_ind_paral,
   cre.ind_nue_des,
   cre.ind_ref,
-
   --DESEMBOLSO
   cre.fec_des_cre,      --fecha
   fec_cierre = EOMONTH(cre.fec_des_cre),                 --fecha de cierre del mes
   mon_des = (case when cre.COD_MON_CRE='01' then cre.mon_des else cre.mon_des*(cast(tc2.tca_fij as decimal(12,6) ))  end),          --monto desembolsado
-
-
   --SECTORISTA
   IND_cambio_asesor = case when cre.cod_sec<>cre.cod_sec_ori then 1 else 0 end,
   cod_sec_ori = case when ind_ref =1 then tab4.cod_sec else his.cod_sec_ori end,
@@ -75,35 +62,27 @@ mxBase <- function(handle){
   WHEN ase.tipo =3  THEN 'LEGAL'   END),
   Asesor_origen = ase.nom_sec,
   ase.vigente,
-
   --AGENCIA/O.I.
   cre.cod_ofi_inf,
   Oficina = ofi.des_ofi_lar,
-
   --MONEDA
   cre.cod_mon_cre,
   Moneda = mon.Nom_Moneda,
-
   --EVALUACIÓN
   efi.efi_cod_usu_res,  --Usuario responsable
   efi.efi_cod_usu_ev,   --Usuario evaluador
   efi.efi_cod_usu,      --Usuario
-
   --OTROS
-
   neg.DIST, --distrito del negocio
   nEdadDesem = year(cre.fec_des_cre) -year(cli.fec_nac), -- edad que tenia el cliente cuando solicitó el crédito
   ciiu2.cod_act_eco, --codigo actividad económica 1
   ciiu2.val_par as val_par1, -- descripción actividad económica 1
   ciiusec2.cod_act_eco_sec, -- código de actividad económica secundaria
   ciiusec2.val_par as val_par2, -- descripción actividad económica secundaria
-
-
   --NÚMERO DE EXCEPCIONES
   num_exc=isnull(tab_excepciones.num_exc ,0)
   from
   rie_app_credito cre
-
   left join rie_app_CREDITO_his his on his.NUM_SOL =cre.NUM_SOL      --oficinas
   left join rie_app_ofi ofi on ofi.cod_ofi =cre.cod_ofi_inf      --oficinas
   left join rie_App_Asesor ase on ase.cod_sec = cre.cod_sec_ori    --asesores
@@ -123,9 +102,7 @@ mxBase <- function(handle){
   inner join
   rie_app_credito cre on cre.num_cre =his.num_cre and month(cre.FEC_DES_cre)= month (his.fec_pro_cla_sbs) and  year(cre.FEC_DES_cre)= year (his.fec_pro_cla_sbs) and ind_ref = 1
   ) tab4 on his.num_cre = tab4.NUM_CRE
-
   left join
-
   (SELECT
   count (cre.num_cre) as num_exc,
   cre.num_Cre
@@ -135,7 +112,6 @@ mxBase <- function(handle){
   INNER JOIN [Rie_App_Excep_Cred  ] om ON cre.NUM_SOL = om.num_sol
   WHERE  om.tipo='EXCEPCION'
   group by cre.num_cre) tab_excepciones on tab_excepciones.NUM_CRE = cre.NUM_CRE     --número de excepciones
-
   left join (select distinct
   rcc.FEC_REp,
   num_emp,
@@ -145,11 +121,9 @@ mxBase <- function(handle){
   Rie_App_Credito cre
   INNER join Rie_App_Cliente cli on cli.COD_CLI = cre.COD_CLI
   INNER join Rie_App_Saldos_Rcc rcc on cli.COD_SBS_CLI = rcc.cod_deu_sbs
-
   where year(rcc.fecha_cierre) =year(cre.fec_des_cre)
   and month(rcc.fecha_cierre)  =month(cre.fec_des_cre)
   ) tab5 on  cre.num_cre = tab5.NUM_CRE
-
   where cre.fec_des_cre>='2015-10-01'
   "
   dfBase <- sqlQuery(channel = handle,
